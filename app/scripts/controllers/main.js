cbed44c1eb6f90e42e25cf237aa9f7ab72f7165c
Alex Yi <ayi822@gmail.com>
2016-06-05 02:11:43 -0700

    added status message, now displays photos in angular cards as a feed (new ones get put on top)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,6 +14,10 @@ angular.module('aggieFeedActivitiesApp')
     $scope.photo;
     $scope.title;
     $scope.search;
+    $scope.photoDateTaken;
+    $scope.cards = [];
+    var numCards = 0;
+    $scope.searching = 'awaiting query';
 
 
     function getRandomInt(min, max) {
@@ -22,10 +26,13 @@ angular.module('aggieFeedActivitiesApp')
 
 
     $scope.setSearch = function() {
-      var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=11485ecf02eaf4f3bedc13056c539453&format=json&nojsoncallback=1&content_type=1&safe_search=1&per_page=50&sort=relevance&extras=url_m&text=' + $scope.search;
+      var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=11485ecf02eaf4f3bedc13056c539453&format=json&nojsoncallback=1&content_type=1&safe_search=1&per_page=50&sort=relevance&extras=url_m, date_taken&text=' + $scope.search;
       var responsePromise = $http.get(url);
+      
+      $scope.searching = 'searching...';
 
       responsePromise.success(function(response) {
+        $scope.searching = 'search complete';
         $scope.photos = response.photos.photo;
       });
     };
@@ -35,6 +42,12 @@ angular.module('aggieFeedActivitiesApp')
       var random = getRandomInt(0, $scope.photos.length);
       $scope.photo = $scope.photos[random].url_m;
       $scope.title = $scope.photos[random].title;
+      $scope.photoDateTaken = $scope.photos[random].datetaken;
+      $scope.cards.push({});
+      $scope.cards[numCards].photo = $scope.photo;
+      $scope.cards[numCards].title = $scope.title;
+      $scope.cards[numCards].photoDateTaken = $scope.photoDateTaken;
+      numCards += 1;
     };
 
 
