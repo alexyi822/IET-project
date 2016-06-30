e883146e61da9aec22f46d912de9f2ec64c87fb5
Alex Yi <ayi822@gmail.com>
2016-06-29 17:52:14 -0700

    added feature to save albums, hardcoded to handle up to 2 albums for right now

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,14 +10,27 @@
    * Controller of the aggieFeedActivitiesApp
    */
   angular.module('aggieFeedActivitiesApp')
-    .controller('MainCtrl', ['$scope', '$http', 'getPhotosService', function($scope, $http, getPhotosService) {
+    .controller('MainCtrl', ['$scope', '$http','$mdDialog', 'getPhotosService', function($scope, $http, $mdDialog, getPhotosService) {
       
       //STEP 2 (refactor ajax call to a service):
       $scope.search = '';
       $scope.cards = [];
       var photos = [];
       $scope.searching = 'Status: awaiting query';
-      $scope.filterOption = "all";
+      $scope.filterOption = 'all';
+      $scope.createAlbum = false;
+      $scope.albumName = '';
+
+      $scope.albums = [
+        // {
+        //   'name': 'San Francisco',
+        //   'cards': [],
+        // },
+        // {
+        //   'name': 'Ferrari',
+        //   'cards': [],
+        // }
+      ];
       // var numCards = 0;
 
       function getRandomInt(min, max) {
@@ -41,9 +54,47 @@
           "url" : photos[random].activity.object.contentImage.dimensions.normal.url,
           "title" : photos[random].activity.object.content,
           "date" : photos[random].activity.object.date,
-          "liked" : false
+          "liked" : false,
+          "album": ''
         });
       };
+
+      $scope.showField = function() {
+        if (!$scope.createAlbum){
+          $scope.createAlbum = true;
+        } else {
+          $scope.createAlbum = false;
+        }
+      };
+
+      $scope.clearSaved = function() {
+        var card;
+        for (card in $scope.cards) {
+          $scope.cards[card].liked = false;
+        }
+      }
+
+      $scope.addAlbum = function() {
+
+        var temp = [];
+        var card;
+        for (card in $scope.cards) {
+          if ($scope.cards[card].liked) {
+            temp.push($scope.cards[card]);
+            $scope.cards[card].album = $scope.albumName;
+          }
+        }
+        $scope.albums.push({
+          'name': $scope.albumName,
+          'cards': temp
+        });
+
+        $scope.createAlbum = false;
+      };
+
+      $scope.clearFeed = function() {
+        $scope.cards = [];
+      }
 
 
 
